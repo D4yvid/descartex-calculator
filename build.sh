fafffdd59b29a2d78a40751be2bcfeb51443c620
Dayvid Albuquerque <kkd4yv1d@gmail.com>
2025-02-09 16:19:50 -0300

    fix(build.sh): fixed build script for UNIX systems
    Made the build script work the same as the `./build.bat` script.

--- build.sh
+++ build.sh
@@ -3,8 +3,13 @@
 # Enable tracing and strict mode
 set -xeuo pipefail
 
+# Reboot target device using picotool
 picotool reboot -f -u
 
+# Build project
 cmake --build build
 
-picotool load -F -x ./build/
+# Load the compiled file into the Pico
+while ! picotool load -F -x ./build/descartex.elf 2> /dev/null; do
+	wait
+done
